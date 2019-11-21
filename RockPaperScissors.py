7532c30ba0438e3d76a84f846c43441433a89b26
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2019-11-21 20:15:07 +0100

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,21 +1,6 @@
 from tkinter import *
 from random import *
 
-window = Tk()
-window.geometry("450x250")
-window.resizable(0, 0)
-window.title("Rock, paper, scissors")
-topFrame = Frame(window)
-topFrame.pack()
-bottomFrame = Frame(window)
-bottomFrame.pack(side=BOTTOM)
-computerLabel = Label(bottomFrame, text="", font=("Courier", 14))
-computerLabel.pack()
-resultLabel = Label(bottomFrame, text="Welcome to the Rock, paper, scissors game\n", font=("Times New Roman", 18))
-resultLabel.pack()
-scoreLabel = Label(bottomFrame, font=("Times New Roman", 18, 'bold'))
-scoreLabel.pack()
-
 computer_score = 0
 player_score = 0
 
@@ -49,14 +34,26 @@ def button_scissors_on_click():
     game(3)
 
 
+window = Tk()
+window.geometry("450x250")
+window.resizable(0, 0)
+window.title("Rock, paper, scissors")
+topFrame = Frame(window)
+topFrame.pack()
+bottomFrame = Frame(window)
+bottomFrame.pack(side=BOTTOM)
+computerLabel = Label(bottomFrame, text="", font=("Courier", 14))
+computerLabel.pack()
+resultLabel = Label(bottomFrame, text="Welcome to the Rock, paper, scissors game\n", font=("Times New Roman", 18))
+resultLabel.pack()
+scoreLabel = Label(bottomFrame, font=("Times New Roman", 18, 'bold'))
+scoreLabel.pack()
 rock = PhotoImage(file="images/100x100/Rock.png")
 paper = PhotoImage(file="images/100x100/Paper.png")
 scissors = PhotoImage(file="images/100x100/Scissors.png")
-
 buttonRock = Button(topFrame, height=100, width=100, command=button_rock_on_click, image=rock)
 buttonPaper = Button(topFrame, height=100, width=100, command=button_paper_on_click, image=paper)
 buttonScissors = Button(topFrame, height=100, width=100, command=button_scissors_on_click, image=scissors)
-
 buttonRock.pack(side=LEFT)
 buttonPaper.pack(side=LEFT)
 buttonScissors.pack(side=LEFT)
